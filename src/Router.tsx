96342705ed51aa219b72bd2f527621fe9c4351cb
moonjiyoung <umarg228@naver.com>
2023-08-24 19:08:41 +0900

    feat: params연결한 Link

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,10 +5,7 @@ import About from "./screens/About";
 import Root from "./Root"
 import NotFound from "./screens/NotFound";
 import User from "./screens/users/User";
-
-
-
-
+import Followers from "./screens/users/Followers";
 
 
 // function Router(){
@@ -38,6 +35,12 @@ const router = createBrowserRouter([
             {
                 path:"users/:userId",
                 element: <User />,
+                children: [
+                    {
+                        path:"followers",
+                        element: <Followers />
+                    }
+                ]
             }
         ],
         errorElement: <NotFound />
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { Outlet, useParams, Link } from "react-router-dom";
 import { users } from "../../db";
-
 
 
 function User(){
     const {userId} = useParams();
     
-    return <h1>User with id {userId} is named: {users[Number(userId) -1].name}</h1>
+    return (
+    <>
+    <h1>User with id {userId} is named: {users[Number(userId) -1].name}</h1>
+    <hr/>
+    <Link to="followers">See followers</Link>
+    <Outlet />
+    </>
+    )
 }
 export default User;
